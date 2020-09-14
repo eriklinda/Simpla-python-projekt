af0fcfca2896d1eaf5aa7087175a9746d0169c60
eriklinda <51276656+eriklinda@users.noreply.github.com>
2020-09-14 18:36:21 +0200

    Gissnings spel
    Ett simpelt spel där man ska gissa på ett tal/siffra mellan 1-50, datorn talar sen om det rätta svaret alt ifall det var rätt eller fel!

--- GissaRatt.py
+++ GissaRatt.py
@@ -7,14 +7,17 @@ Start = input("Hej vill du spela ett spel? Ange Ja eller Nej: ")
 def spela_spelet():
     # Ställer frågan till spelaren samt sparar ner användarens svar
     svar = int(input("Tänk på ett tal mellan 1-50: "))
+    #Här deklareras och genereras det rätta svaret
     ratt_svar = random.randint(1,50)
+
+    #Här kollar programmet ifall svaret är rätt samt ev talar om att det var rätt alt fel
     if svar == ratt_svar:
         print(("Bra jobbat! Ditt svar va {} och det var rätt!").format(svar))
     else:
         print(("Tyvärr du hade fel, ditt svar var {} och rätt svar var {}!").format(svar, ratt_svar))
 
 
-
+#Här startas spelet ifall användaren angett Ja eller Nej
 if Start == "Ja":
     spela_spelet()
 if Start == "Nej":
