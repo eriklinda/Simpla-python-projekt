f3a7a6a97889e05d42cbb1ea92d41a3b55a02115
eriklinda <51276656+eriklinda@users.noreply.github.com>
2020-09-16 16:39:42 +0200

    En funktion som kollar relationen till luke
    En simpel funktion som lämnar tillbaka en sträng som printas ut beroende på vilket namn man anger. Pappa, syster eller brother in law

---
+++ KollaRelationen.py
@@ -0,0 +1,9 @@
+def Relation_till_luke(name):
+    if name == "Darth Vader":
+        return print("Luke, i am your father!")
+    if name == "Leia":
+        return print("Luke, i am your sister!")
+    if name == "Han":
+        return print("Luke, i am your brother in law.")
+
+Relation_till_luke("Han")
